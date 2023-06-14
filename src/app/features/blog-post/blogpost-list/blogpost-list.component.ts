cd46a0a954ca50872ae12895470bea1bf5956eb8
sameer8saini <sameer8saini@gmail.com>
2023-06-14 20:30:38 +1200

    End Of Section 8 - Get All BlogPosts

--- src/app/features/blog-post/blogpost-list/blogpost-list.component.ts
+++ src/app/features/blog-post/blogpost-list/blogpost-list.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BlogPostService } from '../services/blog-post.service';
+import { Observable } from 'rxjs';
+import { BlogPost } from '../models/blog-post.model';
 
 @Component({
   selector: 'app-blogpost-list',
   templateUrl: './blogpost-list.component.html',
   styleUrls: ['./blogpost-list.component.css']
 })
-export class BlogpostListComponent {
+export class BlogpostListComponent implements OnInit {
+
+  blogPosts$?: Observable<BlogPost[]>;
+
+  constructor(private blogPostService: BlogPostService) {
+
+  }
+
+  ngOnInit(): void {
+    // get all blog posts from API
+    this.blogPosts$ = this.blogPostService.getAllBlogPosts();
+  }
 
 }
--- src/app/features/blog-post/services/blog-post.service.ts
+++ src/app/features/blog-post/services/blog-post.service.ts
@@ -15,4 +15,8 @@ export class BlogPostService {
   createBlogPost(data: AddBlogPost) : Observable<BlogPost> {
     return this.http.post<BlogPost>(`${environment.apiBaseUrl}/api/blogposts`, data);
   }
+
+  getAllBlogPosts() : Observable<BlogPost[]> {
+    return this.http.get<BlogPost[]>(`${environment.apiBaseUrl}/api/blogposts`);
+  }
 }
